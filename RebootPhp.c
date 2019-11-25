b95443c9bad85047d8739813513de6d3f3916a24
Jdupont <36246708+Jdupont31@users.noreply.github.com>
2019-11-25 18:35:36 +0100

    Add files via upload

---
+++ RebootPhp.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+ * Compiler : gcc -Wall RebootPhp.c -o RebootPhp
+ * chown www-data:www-data RebootPhp
+ * chmod 777 RebootPhp
+ */
+
+int main() {
+   setuid(0); // Passe en uid root, 0
+   sleep(5);
+   char *command = "/sbin/reboot"; // POinteur
+   execl(command, command, NULL); // Premier command qui se situe dans le Path, le deuxième argument est la commande
+   return 0; // Evite l'erreur (since never returns)
+}
